b6411353fb00ab182a8191ba6274fda0ed7d30d3
kalyankrishna768 <132119666+kalyankrishna768@users.noreply.github.com>
2025-04-01 11:43:30 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-        <p>MetaTicket. All Rights Reserved | 
-            <a href="privacy.php" style="color: rgb(100, 181, 246); text-decoration: none;">Privacy Policy</a> | 
-            <a href="contact.php" style="color: rgb(100, 181, 246); text-decoration: none;">Contact Us</a>
-        </p>
-    </div>
